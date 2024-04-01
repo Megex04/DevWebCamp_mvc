28c084e6cb4a73d3a0ccc1a9a64c55cd8c0d8897
Miguel La Cunza <miantolalfa04@gmail.com>
2024-04-01 18:04:08 -0500

    Add routing to frontend public pages, and developed 'about' and 'paquetes' pages

---
+++ views/paginas/devwebcamp.php
@@ -0,0 +1,22 @@
+<main class="devwebcamp">
+    <h2 class="devwebcamp__heading"><?php echo $titulo ?></h2>
+    <p class="devwebcamp__descripcion">Conoce la conferencia más importante de Latinoamérica</p>
+
+    <div class="devwebcamp__grid">
+        <div class="devwebcamp__imagen">
+            <picture>
+                <source srcset="build/img/sobre_devwebcamp.avif" type="image/avif">
+                <source srcset="build/img/sobre_devwebcamp.webp" type="image/webp">
+                <img loading="lazy" width="200" height="300" src="build/img/sobre_devwebcamp.jpg" alt="Imagen DevWebCamp">
+            </picture>
+        </div>
+        <div class="devwebcamp__contenido">
+            <p class="devwebcamp__texto">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam expedita cupiditate architecto? Magni, facere porro.
+            </p>
+            <p class="devwebcamp__texto">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae repellendus veniam quas consequatur temporibus, mollitia impedit ducimus illum incidunt asperiores! Exercitationem, reiciendis facilis!    
+            </p>
+        </div>
+    </div>
+</main>
